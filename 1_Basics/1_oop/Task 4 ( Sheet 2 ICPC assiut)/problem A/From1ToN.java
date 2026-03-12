7984e5115ecfceff25f03242256c431bb5151d9f
John-wagdy <johnwagdy2005@gmail.com>
2026-03-12 21:20:33 +0200

    print from 1 to n

---
+++ 1_Basics/1_oop/Task 4 ( Sheet 2 ICPC assiut)/problem A/From1ToN.java
@@ -0,0 +1,18 @@
+public class From1ToN {
+    private int number;
+
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void printNumbers (){
+        for (int i = 1; i <= number; i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+
+}
---
+++ 1_Basics/1_oop/Task 4 ( Sheet 2 ICPC assiut)/problem A/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     From1ToN from1ToN = new From1ToN();
+     from1ToN.setNumber(scanner.nextInt());
+     from1ToN.printNumbers();
+    }
+}
+
+
+
+
+
+
+
